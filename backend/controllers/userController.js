919bed845032e2f2772c28a94484dfff4ff9d3d3
McTyler Tong <mctylert245@gmail.com>
2022-06-17 14:32:59 -0500

    4th Edit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,7 @@
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 const User = require('../models/userModel')
 
 const registerUser = asyncHandler(async (req, res) => {
@@ -30,7 +32,8 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(201).json({
             _id: user._id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     } else {
         res.status(400)
@@ -41,13 +44,14 @@ const registerUser = asyncHandler(async (req, res) => {
 const loginUser = asyncHandler(async (req, res) => {
     const {email, password} = req.body
 
-    const user = await User.findOne(email)
+    const user = await User.findOne({email})
 
     if(user && (await bcrypt.compare(password, user.password))) { 
         res.status(200).json({
             _id: user._id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     } else {
         res.status(401)
@@ -55,7 +59,23 @@ const loginUser = asyncHandler(async (req, res) => {
     }
 })
 
+const getMe = asyncHandler(async(req, res) => {
+    const user = {
+        id: req.user._id,
+        email: req.user.email,
+        name: req.user.name
+    }
+    res.status(200).json(user)
+})
+
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    })
+}
+
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    getMe
 }
