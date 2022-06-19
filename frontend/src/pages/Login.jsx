f2946390ced058392bd93dbabaf5b6fcf3ae54a9
McTyler Tong <mctylert245@gmail.com>
2022-06-19 12:00:13 -0500

    9th Edit

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,10 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import {FaSignInAlt} from 'react-icons/fa'
 import {toast} from 'react-toastify'
+import { useNavigate } from 'react-router-dom'
 import {useSelector, useDispatch} from 'react-redux'
-import {login} from '../features/auth/authSlice'
+import {login, reset} from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Login() {
     const [formData, setFormData] = useState({
@@ -13,8 +15,25 @@ function Login() {
     const { email, password } = formData
 
     const dispatch = useDispatch()
+    const navigate = useNavigate()
 
-    const {user, isLoading, isSuccess, message} = useSelector(state => state.auth)
+    const {user, isLoading, isError, isSuccess, message} = useSelector(state => state.auth)
+
+    useEffect(() => {
+        if(isError) {
+            toast.error(message)
+        }
+
+        if(isSuccess || user) {
+            navigate('/')
+        }
+
+        dispatch(reset())
+    }, [isError, isSuccess, user, message, navigate, dispatch])
+
+    if(isLoading) {
+        return <Spinner />
+    }
 
     const onChange = e => {
         setFormData((prevState) => ({
