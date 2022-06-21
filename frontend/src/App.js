66d521d4e3c72262ad4c5913b999ccfe842cf88e
McTyler Tong <mctylert245@gmail.com>
2022-06-20 19:28:24 -0500

    12th Edit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,10 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import NewTicket from './pages/NewTicket';
+import PrivateRoute from './components/PrivateRoute';
+import Tickets from './pages/Tickets';
+import Ticket from './pages/Ticket';
 
 function App() {
   return (
@@ -14,8 +18,17 @@ function App() {
           <Header />
           <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='login' element={<Login />} />
-            <Route path='register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/new-ticket' element={<PrivateRoute />} >
+              <Route path='/new-ticket' element={<NewTicket />} />
+            </Route>
+            <Route path='/tickets' element={<PrivateRoute />} >
+              <Route path='/tickets' element={<Tickets />} />
+            </Route>
+            <Route path='/ticket/:ticketId' element={<PrivateRoute />} >
+              <Route path='/ticket/:ticketId' element={<Ticket />} />
+            </Route>
           </Routes>
         </div>
       </Router>
